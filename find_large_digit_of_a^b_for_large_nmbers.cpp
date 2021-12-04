d2f5febfe4ca36c3cd2ddf4a281b7738b5924576
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-12-04 22:27:37 +0600

    Problem of the day 4/12/2021

---
+++ find_large_digit_of_a^b_for_large_nmbers.cpp
@@ -0,0 +1,21 @@
+class Solution{
+public:
+  int Modulo(int a,string& b){
+      int mod=0;
+      for(int i=0;i<b.length();i++){
+          mod=(mod*10+b[i]-'0')%a;
+      }
+      return mod;
+  }
+  int getLastDigit(string a, string b) {
+        // code here
+        int len_a=a.length(), len_b=b.length();
+        if(len_a == 1 && len_b == 1 && a[0] == '0' && b[0] == '0') return 1;
+        if(len_b == 1 && b[0] == '0') return 1;
+        if(len_a == 1 && a[0] == '0') return 0;
+        
+        int ex=(Modulo(4,b)==0 ? 4 : Modulo(4,b));
+        int res=pow(a[len_a-1]-'0',ex);
+        return res%10;
+    }
+};
