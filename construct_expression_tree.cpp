a0b71f098e4ad3a57730a78c92eafe1c5b14cb69
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-12-27 16:14:56 +0600

    Problem of the day

---
+++ construct_expression_tree.cpp
@@ -0,0 +1,51 @@
+
+//Recursive Approach
+//TC O(n)
+//SC O(n)
+//function to construct expression tree
+et* make(char postfix[],int& end){
+    et* root = new et(postfix[end--]);
+    if(root->value == '+' || root->value == '*' || root->value == '-' || root->value =='/' || root->value == '^'){
+        root->right = make(postfix,end);
+        root->left = make(postfix,end);
+    }
+    return root;
+}
+et* constructTree(char postfix[])
+{
+//code here
+    int end = strlen(postfix)-1;
+    return make(postfix,end);
+}
+
+//Iterative Approach
+//TC O(n)
+//SC O(n)
+//function to construct expression tree
+et* constructTree(char postfix[])
+{
+//code here
+    stack<et*>s;
+    for(int i = 0;i < strlen(postfix); i++){
+        if(isalpha(postfix[i])){
+            et *node = (struct et*)malloc(sizeof(struct et));
+            node->value = postfix[i];
+            node->left = 0;
+            node->right = 0;
+            s.push(node);
+        }else{
+            et *node1, *node2, *node3;
+            node3 = (struct et*)malloc(sizeof(struct et));
+            node3->value = postfix[i];
+            node2 = s.top();
+            node3->right = node2;
+            s.pop();
+            node1 = s.top();
+            node3->left = node1;
+            s.pop();
+            s.push(node3);
+            
+        }
+    }
+    return s.top();
+}
